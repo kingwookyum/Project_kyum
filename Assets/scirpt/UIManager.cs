b36b568ae9d2f06cb657e46bd7118bf5e2318f6b
kingwookyum <153619568+kingwookyum@users.noreply.github.com>
2023-12-20 15:13:08 +0900

    12.20

--- Assets/scirpt/UIManager.cs
+++ Assets/scirpt/UIManager.cs
@@ -1,44 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using UnityEditor.TerrainTools;
 using UnityEngine;
 using UnityEngine.UI;
 public class UIManager : MonoBehaviour
 {
-    public int id;
-    public bool isNPC;
-
-
-    // 루아는 따로 정의 안하면 글로벌(아무데서나 접근)이었음
-    // 지역 변수(local) 따로 해줬을 것
-   // [HideInInspector]
-    //int value = 0;
-
-   //public Image bg;
-    //[SerializeField]
-    //Color _color;
-
-    //public TextMeshProUGUI _text;
     
-    bool isChanged; // 처음 생성되면 0이 기본으로할당 bool 0 = false , 1 = true
-
-    //Sprite test_01; // 기본으로 되어있는 이미지
-    //[SerializeField]
-    //Sprite test_02; // 변경 이미지
-
-    private void Start()
     {
-        //test_01 = bg.sprite;// 1.png라는 사진의 sprite 값을 갖고있다.
 
-       // _color = bg.color;
-        //Debug.Log(testvaule);
-    }
 
-    public int
-    {
+
+
       
   
 }
+
 
 
 
